c8054c135ac3ba6506c6391467ded8fb7c2717da
Oleg Tsyganov <redsmoke19@gmail.com>
2024-10-07 18:32:57 +0400

    Правки первого разработчика

--- source/script/main.js
+++ source/script/main.js
@@ -1,9 +1,9 @@
 import "../scss/style.scss"
-import {iosVhFix} from "./utils/ios-vh-fix.js";
 import {ScrollLock} from "./utils/scroll-lock.js";
 import {initSearch} from "@/script/modules/search.js";
 import {initSort} from "@/script/modules/sort.js";
 import {initFilter} from "@/script/modules/filter.js";
+import {iosVhFix} from "./utils/ios-vh-fix.js";
 
 window.addEventListener('DOMContentLoaded', () => {
   window.scrollLock = new ScrollLock();
